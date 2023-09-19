84fdfa672768163660999aeeaff428eb288b4a0c
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-19 09:39:46 +0900

    titleMessageの変数を宣言する

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 let imagesItems = [...document.querySelectorAll(".img-wrap")];
 console.log(imagesItems);
 let titles = [...document.querySelectorAll("h2")];
+let titleMessage = document.querySelector(".title");
