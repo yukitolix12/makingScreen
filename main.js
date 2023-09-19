96f91a4a4edcda7ac60659b92e0e866ca558a4c5
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-19 09:46:28 +0900

    observe機能作成

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ let imagesItems = [...document.querySelectorAll(".img-wrap")];
 console.log(imagesItems);
 let titles = [...document.querySelectorAll("h2")];
 let titleMessage = document.querySelector(".title");
+
+//どこにいるのか監視する。そして、特定の位置に来たら関数を呼ぶ
+let observer = new IntersectionObserver(setItemActive,options);
