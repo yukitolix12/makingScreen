ef204014034e33398701c92b4a84bf1e919bd42a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-20 14:38:44 +0900

    option定数を作成

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ console.log(imagesItems);
 let titles = [...document.querySelectorAll("h2")];
 let titleMessage = document.querySelector(".title");
 
-//どこにいるのか監視する。そして、特定の位置に来たら関数を呼ぶ
-let observer = new IntersectionObserver(setItemActive,options);
-
-observer.observer(titleMessage);
-
 //監視対象になった瞬間、activeを負荷する関数
 let setItemActive = (entries) => {
     console.log(entries);
 };
+
+let options = {
+
+};
+
+//どこにいるのか監視する。そして、特定の位置に来たら関数を呼ぶ
+let observer = new IntersectionObserver(setItemActive,options);
+
+observer.observe(titleMessage);
+
