2dbedfe6d15d08dc4035edeeaee4288f0abc5b85
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-20 14:55:08 +0900

    remove関数作成、screenの文字が消えるようにする

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ let setItemActive = (entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
             entry.target.classList.add("active");
+        } else {
+            entry.target.classList.remove("active");
         }
     });
 };
